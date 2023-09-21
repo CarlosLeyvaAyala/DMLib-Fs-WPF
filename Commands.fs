a1e2b2d9f69a616b31a362955a1008c1c4060e68
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-09-21 10:37:27 -0600

    Add commands

---
+++ Commands.fs
@@ -0,0 +1,31 @@
+﻿module DMLib_WPF.Commands
+
+open System.Windows.Input
+
+type Cmd =
+    { name: string
+      text: string
+      keyDisplay: string
+      key: Key
+      modifiers: ModifierKeys }
+
+let createCmd typeFunc v =
+    let inputs = InputGestureCollection()
+
+    inputs.Add(KeyGesture(v.key, v.modifiers, v.keyDisplay))
+    |> ignore
+
+    RoutedUICommand(v.text, v.name, typeFunc (), inputs)
+
+let blankCmd =
+    { name = ""
+      text = ""
+      keyDisplay = ""
+      key = Key.None
+      modifiers = ModifierKeys.None }
+
+let basicCmd (create: Cmd -> RoutedUICommand) name text =
+    { blankCmd with
+        name = name
+        text = text }
+    |> create

4fca1942241d63ca00c47f6542fde9af870e3df7
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-09-25 12:09:23 -0600

    Add application context

---
+++ ApplicationContext/ApplicationContext.fs
@@ -0,0 +1,8 @@
+﻿namespace DMLib_WPF.Contexts
+
+open DMLib
+
+type ApplicationContext() =
+    inherit WPFBindable()
+
+    member val WindowsSound = WindowsSoundPlayer() with get, set
---
+++ ApplicationContext/WindowsSoundPlayer.fs
@@ -0,0 +1,28 @@
+﻿namespace DMLib_WPF.Contexts
+
+open System.Media
+
+type SoundEffect =
+    | NoSound
+    | Success
+    | Hint
+    | Error
+
+type WindowsSoundPlayer() =
+    [<CompiledName("Play")>]
+    member _.play sound =
+        (match sound with
+         | NoSound -> fun () -> ()
+         | Success -> SystemSounds.Exclamation.Play
+         | Error -> SystemSounds.Hand.Play
+         | Hint -> SystemSounds.Asterisk.Play)
+            ()
+
+    [<CompiledName("PlaySuccess")>]
+    member t.playSuccess() = t.play Success
+
+    [<CompiledName("PlayHint")>]
+    member t.playHint() = t.play Hint
+
+    [<CompiledName("PlayError")>]
+    member t.playError() = t.play Error

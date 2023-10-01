b5e7c13c57658eec079b0836ac274be14f91b031
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-10-01 15:06:05 -0600

    Add functions

---
+++ IO/File.fs
@@ -0,0 +1,12 @@
+﻿module DMLib_WPF.IO.File
+
+open System.Windows
+open DMLib
+
+let getDroppedFiles (e: DragEventArgs) =
+    match e.Data.GetDataPresent DataFormats.FileDrop with
+    | false -> [||]
+    | true -> e.Data.GetData(DataFormats.FileDrop) :?> (string array)
+
+let getDroppedFile (e: DragEventArgs) =
+    e |> getDroppedFiles |> Array.firstOr ""

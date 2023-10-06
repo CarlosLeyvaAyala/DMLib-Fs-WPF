0c643bf0fd5411a6b2cae0e3dd72e06f2b4045a0
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-10-05 19:29:04 -0600

    Add comparison converter

---
+++ Converters/ComparisonConverter.fs
@@ -0,0 +1,20 @@
+﻿namespace DMLib_WPF.Converters
+
+open System.Windows.Data
+open DMLib.Objects
+
+/// Checks that an object is some value.
+/// Can be used for binding RadioButtons.
+[<ValueConversion(typeof<obj>, typeof<bool>)>]
+type ComparisonConverter() =
+    interface IValueConverter with
+        member _.Convert(value, _, param, _) =
+            match value with
+            | IsNull -> false
+            | o -> o.Equals param
+
+        member _.ConvertBack(value, _, param, _) =
+            match value with
+            | IsNull -> Binding.DoNothing
+            | o when o.Equals true -> param
+            | _ -> Binding.DoNothing

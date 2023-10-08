ed26a53b778227050e45115b6c9cfdfdf7a20a8c
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-10-08 09:56:43 -0600

    Update ComparisonConverter.fs

--- Converters/ComparisonConverter.fs
+++ Converters/ComparisonConverter.fs
@@ -5,6 +5,9 @@ open DMLib.Objects
 
 /// Checks that an object is some value.
 /// Can be used for binding RadioButtons.
+///
+/// https://stackoverflow.com/questions/1317891/simple-wpf-radiobutton-binding
+/// https://stackoverflow.com/questions/397556/how-to-bind-radiobuttons-to-an-enum/406798#406798
 [<ValueConversion(typeof<obj>, typeof<bool>)>]
 type ComparisonConverter() =
     interface IValueConverter with

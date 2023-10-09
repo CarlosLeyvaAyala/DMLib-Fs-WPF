ea29530d930b8921416af817dc01812dabd77e90
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-10-08 22:21:46 -0600

    Add context menu functions

---
+++ Controls/Misc.fs
@@ -0,0 +1,19 @@
+﻿namespace DMLib_WPF.Controls
+
+open System.Windows.Controls
+open System
+open DMLib
+open DMLib.Objects
+
+module ContextMenu =
+    [<CompiledName("GetCaller")>]
+    let getCaller<'a when 'a: not struct and 'a: null and 'a :> Windows.UIElement> (sender: obj) =
+        let nr = nullToResult
+
+        result {
+            let! menu = sender :?> MenuItem |> nr
+            let! ctxMenu = menu.Parent :?> ContextMenu |> nr
+            let! caller = ctxMenu.PlacementTarget :?> 'a |> nr
+            return caller
+        }
+        |> Result.defaultValue null
